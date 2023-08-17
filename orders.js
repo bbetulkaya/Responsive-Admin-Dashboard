1535c089039e4820ae060de4aa1e651b21fb0e5b
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2023-08-17 18:34:34 +0300

    Feat: add orders content with js

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: "Foldable Mini Drone",
+        productNumber: "85631",
+        paymentStatus: "Due",
+        shipping: "Pending"
+    },
+    {
+        productName: "Larvender KF102 Drone",
+        productNumber: "36578",
+        paymentStatus: "Refunded",
+        shipping: "Decline"
+    },
+    {
+        productName: "Ruko F11 Pro Drone",
+        productNumber: "49856",
+        paymentStatus: "Due",
+        shipping: "Pending"
+    },
+    {
+        productName: "Drone with Camera Drone",
+        productNumber: "95478",
+        paymentStatus: "Paid",
+        shipping: "Delivered"
+    },
+    {
+        productName: "GPS 4K Drone",
+        productNumber: "22548",
+        paymentStatus: "Paid",
+        shipping: "Delivered"
+    },
+]
